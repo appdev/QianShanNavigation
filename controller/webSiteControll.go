3642466dbb4f193807d22ce79926579c023fcdd5
zhaoshulin <Aa123456>
2021-03-18 18:00:12 +0800

    - 增加控制器

--- controller/webSiteControll.go
+++ controller/webSiteControll.go
@@ -47,6 +47,7 @@ func AddCategory(c *gin.Context) {
 		return
 	}
 	webSite := model.WebSite{Url: addWeb.Url, UserID: userId, Name: addWeb.Name, Category: addWeb.Category}
+	webSite.Favicon = util.Fetch(addWeb.Url)
 	id, err := webSite.Insert()
 	if err != nil {
 		middleware.ResponseError(c, -1, errors.New("添加失败"))
