464d53d5f03399de0b698caf865f6c95467bd699
zhaoshulin <Aa123456>
2021-06-28 16:18:08 +0800

    - 修改展示展示列表规则

--- src/App.vue
+++ src/App.vue
@@ -25,10 +25,9 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
   display: flex;
   height: 100%;
-  overflow: scroll;
+  overflow-y: scroll;
   flex-direction: column;
 }
 
